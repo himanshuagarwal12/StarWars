a25a6db9af5e2a9a071d4dd0eb64dc99f27ee89f
HIMANSHU R AGARWAL <himanshu59cool@gmail.com>
2020-09-23 14:37:50 +0530

    files starwars

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { ListComponent } from './list/list.component';
+import { ItemComponent } from './item/item.component';
+import { StarWarsService } from './star-wars.service';
+import { LogService } from './log.service';
+import { CreateCharacterComponent } from './create-character/create-character.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TabsComponent,
+    ListComponent,
+    ItemComponent,
+    CreateCharacterComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [StarWarsService, LogService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
